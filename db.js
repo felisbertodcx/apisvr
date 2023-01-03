adb3b0497e6472eb517641fe7fa40454f916d22f
felisbertodcx <97169844+felisbertodcx@users.noreply.github.com>
2023-01-03 13:38:44 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize("test", "root", "root", {
-    host: 'localhost',
-    port: 3307,
+const sequelize = new Sequelize("banc10", "banc10", "Felis1993@", {
+    host: 'banc10.mysql.uhserver.com',
+    port: 3306,
     dialect: 'mysql'
 });
 
